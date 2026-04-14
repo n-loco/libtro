c3b2e3d21419077e4233ebc2c00899534fa8fac5
N_Loco <nicolasrsantos.nloco@proton.me>
2026-04-14 03:06:43 -0300

    adicionar funções tro_conv_str_to_str16 e tro_cnvlloc_str_to_str16 em string.h

---
+++ src/string/unicode.c
@@ -0,0 +1,79 @@
+#include "tro/string.h"
+
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define REPLACEMENT_CHARACTER_U16 0xFFFD
+
+size_t tro_conv_str_to_str16(const char *in, size_t in_len, tro_char16 *out,
+                             size_t out_cap)
+{
+	if (out == NULL || out_cap == 0)
+		return 0;
+	if (in == NULL) {
+		if (out_cap > 1)
+			*out = '\0';
+		return 0;
+	}
+
+	if (in_len == 0)
+		in_len = strlen(in);
+
+	const size_t out_cap_bn = out_cap - 1;
+	size_t written_out      = 0;
+
+	size_t i = 0;
+	while (i < in_len) {
+		const tro_u8code *seq = (const tro_u8code *)(in + i);
+
+		tro_urune rune;
+		i += tro_u8codes_to_urune(seq, &rune);
+
+		tro_u16code surrogates[TRO_MULTI_U16CODE_MAX];
+		size_t surrogates_n = tro_urune_to_u16codes(rune, surrogates);
+
+		const size_t remaining_capacity = out_cap_bn - written_out;
+		if (remaining_capacity < surrogates_n) {
+			out[written_out] = REPLACEMENT_CHARACTER_U16;
+			written_out++;
+			break;
+		}
+
+		out[written_out] = surrogates[0];
+		written_out++;
+
+		if (surrogates_n > 1) {
+			out[written_out] = surrogates[1];
+			written_out++;
+		}
+
+		if (written_out == out_cap_bn)
+			break;
+	}
+
+	out[written_out] = '\0';
+	return written_out;
+}
+
+tro_char16 *tro_cnvlloc_str_to_str16(const char *in, size_t in_len,
+                                     size_t *len_out)
+{
+	if (in == NULL) {
+		*len_out = 0;
+		return NULL;
+	}
+
+	if (in_len == 0)
+		in_len = strlen(in);
+
+	const size_t str_mem =
+	    (tro_str8_urune_len(in, in_len) * sizeof(tro_urune)) + 2;
+	const size_t str_cap = str_mem / 2;
+
+	tro_char16 *str = malloc(str_mem);
+	size_t str_len  = tro_conv_str_to_str16(in, in_len, str, str_cap);
+
+	*len_out = str_len;
+	return str;
+}
---
+++ tests/string/str8_to_str16.c
@@ -0,0 +1,60 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <tro/string.h>
+
+static const char HELLO_WORLD[] = "Olá, mundo! 🌎";
+// clang-format off
+static const tro_char16 HELLO_WORLD_U16[] = {
+	0x004F,         // O
+	0x006C,         // l
+	0x00E1,         // á
+	0x002C,         // ,
+	0x0020,         //  
+	0x006D,         // m
+	0x0075,         // u
+	0x006E,         // n
+	0x0064,         // d
+	0x006F,         // o
+	0x0021,         // !
+	0x0020,         //  
+	0xD83C, 0xDF0E, // 🌎
+	0x0000,         // Terminação da string.
+};
+// clang-format on
+
+#define HELLO_WORLD_U16_LEN 14
+
+#include <stdio.h>
+
+int main(void)
+{
+	const size_t str_mem = 15;
+	const size_t str_cap = 15;
+
+	tro_char16 *str = malloc(str_mem);
+	size_t str_len  = tro_conv_str_to_str16(HELLO_WORLD, 0, str, str_cap);
+
+	bool same_len = str_len == HELLO_WORLD_U16_LEN;
+	bool str_eq   = same_len;
+	if (str_eq) {
+		for (size_t i = 0; i < str_len; i++) {
+			const tro_char16 expected = HELLO_WORLD_U16[i];
+			const tro_char16 got      = str[i];
+			if (expected != got) {
+				fprintf(stderr,
+				        "unmatched surrogates: expected "
+				        "0x%04X, got 0x%04x\n",
+				        expected, got);
+				str_eq = false;
+			}
+		}
+	} else
+		fprintf(stderr, "not same len: expected %zu, got %zu\n",
+		        (size_t)HELLO_WORLD_U16_LEN, str_len);
+
+	free(str);
+
+	return !str_eq;
+}
